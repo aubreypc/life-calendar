0230c439ee6782ca76bc79a970eff28512287e3b
Aubrey Collins <collinsp@allegheny.edu>
2015-09-14 18:33:29 -0400

    added home page

--- LifeCalendar.py
+++ LifeCalendar.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 from Calendar import Calendar
 
 app = Flask("LifeCalender")
 
 @app.route("/")
-def hello():
-	return "Hello World!"
+def index():
+	return render_template("index.html")
 
 @app.route("/calendar/<year>/<month>/<day>")
 def showCalendar(year, month, day):
